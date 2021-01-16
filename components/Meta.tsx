c5575f9242c2b05ab09d20bf146b110befb77a66
Andrew Kay <andrew.kay2@cmft.nhs.uk>
2021-01-16 19:24:49 +0000

    Add self hosted variable font, remove google font

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -11,8 +11,6 @@ const Meta: React.FC<HeadProps> = ({ title, description }) => {
       <title>{title}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" key="title" />
       <meta name="description" content={description ?? "needs description"} />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet" />
     </Head>
   );
 };
