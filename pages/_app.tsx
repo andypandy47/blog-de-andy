41a23054dba5a14709632a1b15f74291ebc7cdc1
Andrew Kay <andrew.kay2@cmft.nhs.uk>
2021-01-16 19:00:23 +0000

    Disable dev tools in _app, delete custom document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,24 @@ import "../styles/globals.scss";
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  if (process.env.NODE_ENV === "production") {
+    if(typeof window !== "undefined") {
+      const noop = (): void => undefined;
+      const DEV_TOOLS = (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__;
+
+      if (typeof DEV_TOOLS === 'object') {
+        for (const [key, value] of Object.entries(DEV_TOOLS)) {
+            DEV_TOOLS[key] = typeof value === 'function' ? noop : null;
+        }
+      }
+      console.log("disabled dev tools");
+    }
+  }
+    
+  if (process.env.NODE_ENV === "development") {
+      console.log("dev environment");
+  }
+
   return <Component {...pageProps} />;
 };
 
